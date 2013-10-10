9812a4a9e4a30a208d77d3b10828a1c174dccd77
Prashanth Pai <ppai@redhat.com>
2013-10-10 15:47:31 +0530

    Add unit tests
    Change-Id: I7bbf74b66c26d0a964fa769bf9c46dd73bd03d73
    Signed-off-by: Prashanth Pai <ppai@redhat.com>
    Reviewed-on: http://review.gluster.org/6067
    Reviewed-by: Luis Pabon <lpabon@redhat.com>
    Tested-by: Luis Pabon <lpabon@redhat.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import setup
 from swiftkerbauth import __version__
+import os
+
+
+# Ugly hack to exclude data_files if running in tox as non root
+def include_data_files():
+    data = [
+        ('/var/www/cgi-bin',
+            ['apachekerbauth/var/www/cgi-bin/swift-auth']),
+        ('/etc/httpd/conf.d',
+            ['apachekerbauth/etc/httpd/conf.d/swift-auth.conf']),
+    ]
+    if os.geteuid() != 0:
+        data = None
+    return data
+
 
 setup(
     name='swiftkerbauth',
@@ -14,6 +44,7 @@ setup(
     packages=['swiftkerbauth'],
     keywords='openstack swift kerberos',
     install_requires=['swift>=1.9.1'],
+    test_suite='nose.collector',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: OpenStack',
@@ -25,14 +56,11 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        ],
-    data_files=[
-        ('/var/www/cgi-bin', ['apachekerbauth/var/www/cgi-bin/swift-auth']),
-        ('/etc/httpd/conf.d', ['apachekerbauth/etc/httpd/conf.d/swift-auth.conf']),
-        ],
+    ],
+    data_files=include_data_files(),
     entry_points={
         'paste.filter_factory': [
             'kerbauth=swiftkerbauth.kerbauth:filter_factory',
-            ],
-        },
-    )
+        ],
+    },
+)
